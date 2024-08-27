c4b8b9047ae70f7a2fef29ab60c09226ac621d46
Walidadebayo <adebayowalid@gmail.com>
2024-08-27 22:18:42 +0100

    chore: Update npm dependency to latest stable version

--- client/src/apis/axios.tsx
+++ client/src/apis/axios.tsx
@@ -1,12 +1,13 @@
 import axios from "axios";
-import dotenv from "dotenv";
-dotenv.config();
+
+const BASE_URL = process.env.REACT_APP_BASE_URL;
 
 const api = axios.create({
-    baseURL: process.env.NODE_ENV === "development" ? "http://localhost:5500" : process.env.BASE_URL,
-    headers: {
-        "Content-Type": "application/json",
-    },
+  baseURL:
+    process.env.NODE_ENV === "development" ? "http://localhost:5500" : BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 export default api;
