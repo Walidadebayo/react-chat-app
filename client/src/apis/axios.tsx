2f49831fa17ad733db02dfe7cc5f9c6dc55f1a42
Walidadebayo <adebayowalid@gmail.com>
2024-09-01 19:30:38 +0100

    chore: Update axios.tsx to use VITE_BASE_URL for baseURL

--- client/src/apis/axios.tsx
+++ client/src/apis/axios.tsx
@@ -1,13 +1,13 @@
 import axios from "axios";
-
-const BASE_URL = process.env.REACT_APP_BASE_URL;
+console.log("Base URL:",  import.meta.env.VITE_BASE_URL);
 
 const api = axios.create({
-  baseURL:
-    process.env.NODE_ENV === "development" ? "http://localhost:5500" : BASE_URL,
+  baseURL:  import.meta.env.VITE_BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
 });
 
 export default api;
+
+
